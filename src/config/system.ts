61e7947b3d64a2f83166420353fb1e834a2500ef
CodeGorgeous <wangshuaishuai@custouch.com>
2023-05-08 16:17:51 +0800

    fix: 🐛 修复影响到vitepress打包的文件
    在打包过程中是处于node环境是没有window对象的, 需要对其进行错误捕获

--- src/config/system.ts
+++ src/config/system.ts
@@ -1,7 +1,12 @@
 // 双端数据标识
 export const twoWayDataSymbol = Symbol("twoWayData");
 // 当前设备的类型: true => 移动端 false => Pc端
-export const systemType = /Mobi|Android|iPhone/i.test(navigator.userAgent);
+export let systemType = true;
+try {
+    systemType = window.navigator ? /Mobi|Android|iPhone/i.test(window.navigator.userAgent) : true;
+} catch (error) {
+    console.log("error", error);
+}
 // 双端事件兼容列表: 二维数组, 0 => Pc端事件, 1 => 移动端事件
 export const systemEventType = [
     ["mousedown", "touchstart"],
