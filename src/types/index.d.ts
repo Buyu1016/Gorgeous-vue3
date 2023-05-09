587b1559bffa66f8e1512cc2d714dc7eca294168
CodeGorgeous <wangshuaishuai@custouch.com>
2023-05-09 16:07:14 +0800

    fix: 去除PromiseReturnType类型

--- src/types/index.d.ts
+++
@@ -1,1 +0,0 @@
-type PromiseReturnType<T> = T extends (...args: any[]) => Promise<infer R> ? R : (T extends (...args: any[]) => infer P ? P : any);
--- src/useFunction/useAsync/index.ts
+++ src/useFunction/useAsync/index.ts
@@ -1,13 +1,15 @@
-import { ref } from "vue";
+import { ref, UnwrapRef } from "vue";
 
-export interface UseAsyncOptions <T extends () => any> {
+export interface UseAsyncOptions <T> {
     // 初始值
-    init?: PromiseReturnType<T>
+    init?: T
     // 错误捕获
     errorHandle?: (err: any) => void
     // 初始化运行函数, 默认执行
     start?: boolean
 }
+
+export type UseAsyncPromise <T> = () => Promise<T>;
 
 /**
  * 提供异步函数管理能力
@@ -15,14 +17,14 @@ export interface UseAsyncOptions <T extends () => any> {
  * @param options 可选项
  * @returns 
  */
-export function useAsync<T extends () => any>(promise: T, options?: UseAsyncOptions<T>){
+export function useAsync<T>(promise: UseAsyncPromise<T>, options?: UseAsyncOptions<T>){
     const loading = ref(false);
-    const value = ref<PromiseReturnType<T> | undefined>(options?.init);
+    const value = ref<T | undefined>(options?.init);
     async function start() {
         loading.value = true;
         await promise()
             .then(result => {
-                value.value = result;
+                value.value = result as UnwrapRef<T>;
             }, options?.errorHandle || ((e) => { // 兜底
                 console.log(`promise error => ${e}`);
             }))
