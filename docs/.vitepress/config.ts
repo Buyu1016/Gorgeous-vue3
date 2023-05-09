a7a7441520c415f049c87c62c0e3f54f15d383a6
CodeGorgeous <wangshuaishuai@custouch.com>
2023-05-09 15:34:04 +0800

    feat: 文档首页新增团队成员区域与项目介绍区域, 并修改调整vitepress相关配置项

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,49 +1,49 @@
 import path from "path";
 import fs from "fs";
+import { defineConfig } from "vitepress"
 
-const nav = [
-    { text: "Npm", link: "https://www.npmjs.com/package/@codegorgeous/gorgeous-ui" },
-    { text: "Github", link: "https://github.com/CodeGorgeous/GorgeousUI" },
-    { text: "Issues", link: "https://github.com/CodeGorgeous/GorgeousUI/issues" }
-]
-
-const sidebar = [
-    {
-        text: "起步",
-        items: [
-            { text: '快速开始', link: '/guide/quickStart' }, 
-        ]
-    },
-    {
-        text: "Components",
-        items: [
-            { text: '浮动按钮 / FloatButton', link: '/components/FloatButton' }, 
-            { text: '图片 / Image', link: '/components/Image' }, 
-            { text: '输入框 / Input', link: '/components/Input' }, 
-        ]
-    },
-    {
-        text: "组合式API",
-        items: fs.readdirSync(path.resolve(__filename, "..", "..", "useFunction")).map(name => {
-            const _name = name.split(".")[0];
-            return {
-                text: _name,
-                link: `/useFunction/${_name}`
-            }
-        })
-    }
-]
-
-const config = {
+const config = defineConfig({
     title: "Gorgeous",
     description: "CodeGorgeous自行搭建的一套Vue3相关库",
     lang: "zh",
     themeConfig: {
-        nav,
-        sidebar,
+        nav: [
+            { text: "Npm", link: "https://www.npmjs.com/package/@codegorgeous/gorgeous-ui" },
+            { text: "Github", link: "https://github.com/CodeGorgeous/GorgeousUI" },
+            { text: "Issues", link: "https://github.com/CodeGorgeous/GorgeousUI/issues" }
+        ],
+        sidebar: [
+            {
+                text: "起步",
+                items: [
+                    { text: '快速开始', link: '/guide/quickStart' }, 
+                ]
+            },
+            {
+                text: "Components",
+                items: [
+                    { text: '浮动按钮 / FloatButton', link: '/components/FloatButton' }, 
+                    { text: '图片 / Image', link: '/components/Image' }, 
+                    { text: '输入框 / Input', link: '/components/Input' }, 
+                ]
+            },
+            {
+                text: "组合式API",
+                items: fs.readdirSync(path.resolve(__filename, "..", "..", "useFunction")).map(name => {
+                    const _name = name.split(".")[0];
+                    return {
+                        text: _name,
+                        link: `/useFunction/${_name}`
+                    }
+                })
+            }
+        ],
         search: {
             provider: 'local'
-        }
+        },
+        outlineTitle: "目录",
+        lastUpdatedText: document.lastModified,
     }
-}
+});
+
 export default config
--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,29 @@
 <!-- <h1>Gorgeous</h1> -->
 # Gorgeous<span style="font-size: 16px">`vue3`</span>
+
+## 项目介绍
+一个面向Vue3项目工程的轮子工程
+
+## 团队成员
+
+<VPTeamMembers size="small" :members="members" />
+
+<script setup>
+import { VPTeamMembers } from 'vitepress/theme'
+
+const members = [
+  {
+    avatar: 'https://avatars.githubusercontent.com/u/64412325?v=4',
+    name: 'CodeGorgeous',
+    title: '创建者',
+    links: [
+      { icon: 'github', link: 'https://github.com/CodeGorgeous' },
+      { icon: {
+            svg: '<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-ea893728=""><path fill="currentColor" d="M512 128 128 447.936V896h255.936V640H640v256h255.936V447.936z"></path></svg>'
+        },
+        link: "http://codegorgeous.top/"
+      }
+    ]
+  },
+]
+</script>
